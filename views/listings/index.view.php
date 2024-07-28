3083a140714b4574455c82944bfb9396bc5cbbc4
Caglar Cetinceviz <caglarcetinceviz@gmail.com>
2024-07-28 21:34:29 +0100

    end of section 9

--- views/listings/index.view.php
+++ views/listings/index.view.php
@@ -1,12 +1,7 @@
-<!-- Head -->
 <?= loadPartial('head') ?>
-<!-- Nav -->
 <?= loadPartial('navbar') ?>
-<!-- Showcase -->
-<?= loadPartial('showcase-search') ?>
-<!-- Top Banner -->
 <?= loadPartial('top-banner') ?>
-<!-- Job Listings -->
+
 <section>
     <div class="container mx-auto p-4 mt-4">
         <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">Recent Jobs</div>
@@ -160,13 +155,7 @@
                 </div>
             </div>
         </div>
-        <a href="listings" class="block text-xl text-center">
-            <i class="fa fa-arrow-alt-circle-right"></i>
-            Show All Jobs
-        </a>
 </section>
 
-<!-- Bottom Banner -->
 <?= loadPartial('bottom-banner') ?>
-<!-- footer -->
 <?= loadPartial('footer') ?>
