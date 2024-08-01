65fae3994ebae107532a3c48460c6035a6f76942
Caglar Cetinceviz <caglarcetinceviz@gmail.com>
2024-08-01 12:26:47 +0100

    end of section 10

--- helpers.php
+++ helpers.php
@@ -18,11 +18,12 @@ function basePath($path = '')
  * @return void
  * 
  */
-function loadView($name)
+function loadView($name, $data = [])
 {
     $viewPath = basePath("views/{$name}.view.php");
 
     if (file_exists($viewPath)) {
+        extract($data);
         require $viewPath;
     } else {
         echo "View '{$name} not found!'";
@@ -70,4 +71,15 @@ function inspectAndDie($value)
     echo '<pre>';
     die(var_dump($value));
     echo '</pre>';
+}
+
+/**
+ * Format Salary
+ *
+ * @param string $salary
+ * @return string $formatted Salary
+ */
+function formatSalary($salary)
+{
+    return '$' . number_format(floatval($salary));
 }
